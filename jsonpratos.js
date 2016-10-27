c7be684359612ca0c2acf2294670e1a3bddb05d1
yaslins <yasmimlins2010@hotmail.com>
2016-10-27 07:32:54 -0300

    iniciando pratos

---
+++ jsonpratos.js
@@ -0,0 +1,33 @@
+   pratos = {
+  "cheeseburger_carne": {
+    "nome": "Cheeseburger de Carne",
+    "preco": 7.99,
+    "descricao": "Deliciosa carne de boa de 1º qualidade grelhada no fogo (de verdade!) com saboroso queijo cheddar duplo derretido. Para completar, alface americana...",
+    "acompanhamentos": "fritas preparadas à perfeição com estupendo molho barbecue",
+    "imagen": "cheesebuger.jpg"
+  },
+  "cheeseburger_frango": {
+    "nome": "Cheeseburger de Carne",
+    "preco": 7.99,
+    "descricao": "Deliciosa carne de boa de 1º qualidade grelhada no fogo (de verdade!) com saboroso queijo cheddar duplo derretido. Para completar, alface americana...",
+    "acompanhamentos": "fritas preparadas à perfeição com estupendo molho barbecue"
+    "imagen":"Chicken.jpg"
+  },
+    "fritas": {
+      "nome": "Fritas",
+      "preco": 4.50,
+      "descricao":"Batatas fritas à perfeição, cobertas com mix de queijos e bacon crocante."
+      "acompanhamentos": " Servida com delicioso molho Ranch."
+      "imagen":"fritas.jpg"
+    },
+
+    "refrigerantes":{
+      "nome":" Refrigerantes",
+      "preco": 6.66,
+      "descricao":"Gostoso",
+      "acompanhamento":"Limão, gelo",
+      "imagen":"refir.jpg"
+    },
+
+    ""
+}
