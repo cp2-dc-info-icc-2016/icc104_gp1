13beff8c4e5d39512afd31e3239811e1d74896f1
yaslins <yasmimlins2010@hotmail.com>
2017-02-02 10:14:15 -0300

    todas as imagens

--- jsonpratos.js
+++ jsonpratos.js
@@ -1,24 +1,24 @@
-   pratos = {
-  "cheeseburger_carne": {
+pratos = {
+  "cheeseburger_Carne": {
     "nome": "Cheeseburger de Carne",
     "preco": 7.99,
     "descricao": "Deliciosa carne de boa de 1º qualidade grelhada no fogo (de verdade!) com saboroso queijo cheddar duplo derretido. Para completar, alface americana...",
-    "acompanhamentos": "fritas preparadas à perfeição com estupendo molho barbecue",
-    "imagen": "cheesebuger.jpg"
+    "acompanhamento": "fritas preparadas à perfeição com estupendo molho barbecue",
+    "imagem": "cheeseburger.jpg"
   },
-  "cheeseburger_frango": {
-    "nome": "Cheeseburger de Carne",
+  "cheeseburger_Frango": {
+    "nome": "Cheeseburger de Frango",
     "preco": 7.99,
     "descricao": "Deliciosa carne de boa de 1º qualidade grelhada no fogo (de verdade!) com saboroso queijo cheddar duplo derretido. Para completar, alface americana...",
-    "acompanhamentos": "fritas preparadas à perfeição com estupendo molho barbecue"
-    "imagen":"Chicken.jpg"
+    "acompanhamento": "fritas preparadas à perfeição com estupendo molho barbecue",
+    "imagem":"Chicken.jpg"
   },
     "fritas": {
       "nome": "Fritas",
       "preco": 4.50,
-      "descricao":"Batatas fritas à perfeição, cobertas com mix de queijos e bacon crocante."
-      "acompanhamentos": " Servida com delicioso molho Ranch."
-      "imagen":"fritas.jpg"
+      "descricao":"Batatas fritas à perfeição, cobertas com mix de queijos e bacon crocante.",
+      "acompanhamento": " Servida com delicioso molho Ranch.",
+      "imagem":"fritas.jpg"
     },
 
     "refrigerantes":{
@@ -26,8 +26,150 @@
       "preco": 6.66,
       "descricao":"Gostoso",
       "acompanhamento":"Limão, gelo",
-      "imagen":"refir.jpg"
+      "imagem":"refir.jpg"
     },
 
-    ""
-}
+    "flurry": {
+    "nome":"Mix Flurry",
+    "preco":5.99,
+    "acompanhamento":"",
+    "descricao":"Creme delicioso com confeito de chocolate",
+    "imagem":"flurry.jpg"
+  },
+
+  "salada_De_Atum": {
+    "nome": "Salada de Atum",
+    "preco": 5.50,
+    "acompanhamento":"Molhos variados = rosê, ketchup, mostarda e maionese.",
+    "descricao":"Folhas frescas e tomates suculentos, com atum selecionado",
+    "imagem":"atum.jpg"
+  },
+   "big_Explosao_cheddar": {
+     "nome":"Hamburguer 'Big Explosão Cheddar'",
+     "preco": 14.50,
+     "acompanhamento":"Ketchup, Mostarda e Maionese",
+     "descricao":"Pão '9 Grãos' com Gergelim, acompanhado de picanha e queijo cheddar derretido que explode na sua boca... tipo o big bang, morô?",
+     "imagem":"cheddar.jpg"
+   },
+
+   "sanduich_Fish":{
+     "nome": "Sanduich Fish",
+     "preco": 12.00,
+     "descricao": "Filé de peixe fresquinho, com pão caseiro e salada",
+     "acompanhamento": " Molho rosê",
+     "imagem":"peixe.jpg"
+   },
+
+   "dell_Valle":{
+    "nome":"Dell Valle",
+    "preco": 5.50,
+    "acompanhamento":"",
+    "descricao":"Sabores: Morango,uva,maçã, manga ,goiaba ,pessego, abacaxi e laranja",
+    "imagem":"dell.jpg"
+  },
+     "danoninho":{
+       "nome":"danoninho",
+       "preco":2.00,
+       "acompanhamento":"",
+       "descricao":"sabores: Morango e Chocolate",
+       "imagem":"danoninho.jpg"
+     },
+
+     "salada_Caesar":{
+       "nome":"Salada Caesar",
+       "preco":7.90 ,
+       "acompanhameto":"Molho Caesar",
+       "descricao":"Delicioso mix de folhas com frango grill ou crispy, queijo parmesão ralado e o toque único dos croutons",
+       "imagem":"caesar.jpg"
+     },
+
+     "hamburguer_Egg_Bacon":{
+       "nome": "Hamburguer Egg Bacon",
+       "preco":9.60,
+       "acompanhamento":"",
+       "descricao":"Dois hambúrgueres da melhor carne 100% bovina acompanhados por queijo cheddar em fatias, cebola roxa, bacon em tiras, picles, mostarda, ketchup no pão Angus",
+       "imagem":"angus-bacon.jpg"
+     },
+
+     "scandalosamente_Delicioso":{
+       "nome":"Escandalosamente Delicioso",
+       "preco":8.00,
+       "acompanhamento":"",
+       "descricao":"Delicioso hamburguer de frango, bacon, cebola crispy, alface , saboroso molho, em um pão quadrado com gergelim e bacon.",
+       "imagem":"cbo.jpg"
+     },
+
+     "nuggets":{
+       "nome":"Nuggets",
+       "preco":9.50,
+       "acompanhamento":"10 unidades + 1 molho",
+       "descricao":"Pedaços de peito de frango empanados. Uma delícia para comer com os amigos, ou esconder deles!.",
+       "imagem":"Brie-Nuggets-Russia.jpg"
+     },
+
+     "agua_Mineral":{
+       "nome":"Agua Mineral ",
+       "preco":3.50,
+       "acompanhamento":"",
+       "descricao":"Para quem gosta de matar a sede do jeito mais natural que existe!Levíssima, saudável e refrescante",
+       "imagem":"agua.jpg"
+     },
+
+     "top_Sundae":{
+       "nome":"Top Sundae",
+       "preco": 5.50,
+       "acompanhamento":"O canudo completa a tentação.",
+       "descricao":"Uma supercamada de cobertura de chocolate, caramelo ou morango e ainda por cima com farofa de paçoca. O biju completa a tentação",
+       "imagem":"top-sundae.jpg"
+     },
+
+     "potato_Salad":{
+       "nome":"POTATO SALAD",
+       "preco": 5.80,
+       "acompanhamento":"acompanhada de uma deliciosa Jacket Potato.",
+       "descricao":"Nossa Caesar Salad, Salada da Casa ou El Ranchito Salad ",
+       "imagem":"potatosalad.jpg"
+     },
+     "hamburguer_Bacon_Honey_Mustard":{
+       "nome":"Hamburguer Bacon Honey Mustard",
+       "preco":35.90,
+       "acompanhamento":"Deliciosas fritas, Bacon em tiras e Honey Mustard",
+       "descricao":"Bem ao estilo de Louisiana, com um toque do delicioso mel, que dão a esse hambúrguer de 200g um sabor único. Servido com alface, tomate, queijo, maionese. ",
+       "imagem":"FullSizeRender.jpg"
+     },
+     "chicken_Supreme":{
+       "nome":"CHICKEN SUPREME",
+       "preco": 19.50,
+       "acompanhamento":"",
+       "descricao":"Carne de frango, molho delicioso, cebola crispy, tomate, alface fresquinha e queijo Emental num saboroso pão integral.",
+       "imagem":"chicken-supreme.jpg"
+     },
+     "tomatinho":{
+       "nome":"Tomatinho",
+       "preco": 5.80,
+       "acompanhamento":"",
+       "descricao":" Tomatinhos vermelhinhos e saborosos como novo acompanhamento no FoodMix",
+       "imagem":"tomatinho.jpg"
+     },
+     "chocolate_Quente":{
+       "nome":" Chocolate quente",
+       "preco":6.00,
+       "acompanhamento":"",
+       "descricao":"Segundo recentes pesquisas realizadas em vários países, não existe nenhuma pessoa que não goste de chocolate. Estamos desconfiados que nosso chocolate quente tem um pouco a ver com isso",
+       "imagem":"cf_quente.jpg"
+     },
+     "pao_De_Queijo":{
+     "nome":"Pao de queijo",
+     "preco":2.25,
+     "acompanhamento":"",
+     "descricao":"Muito queijo, mas muito queijo mesmo! Perfeito para começar o dia. Quem já provou costuma dizer de boca cheia: 'Mnfmn nmfm gmlump nmhm mhmmhb'. Traduzindo: 'Não adianta pedir um pedaço que eu não dou de jeito nenhum' ",
+     "imagem":"pao-de-queijo.jpg"
+   },
+   "saladas_Com_Graos":{
+     "nome":"Saladas com grãos",
+     "preco":23.50,
+     "acompanhamento":"COM FRUTAS, CASTANHAS E FOLHAS",
+     "descricao":"Preparado como o feijão nosso de cada dia, como feijoada, com bacalhau, com dobradinha",
+     "imagem":"grao.jpg"
+   }
+ }
